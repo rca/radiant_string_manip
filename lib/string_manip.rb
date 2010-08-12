814d442e6e7c2e8ff14cf6bb752c0860d8a04fde
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2010-08-12 16:46:41 -0700

    added simple_xml_escape().

--- lib/string_manip.rb
+++ lib/string_manip.rb
@@ -11,4 +11,15 @@ module StringManip
     what_re = Regexp.new(tag.attr['what'])
     tag.expand.gsub(what_re, tag.attr['with'])
   end
+
+  desc "Escape characters not allowed in XML elements (currently &, <, >)"
+  tag "simple_xml_escape" do |tag|
+    expanded = tag.expand
+
+    expanded = expanded.gsub(/&/, '&amp;')
+    expanded = expanded.gsub(/</, '&lt;')
+    expanded = expanded.gsub(/>/, '&gt;')
+
+    expanded
+  end
 end
